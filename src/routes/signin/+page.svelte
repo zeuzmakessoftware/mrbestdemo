159307eba86a782a837a757de8c5d300a0518188
zeuzmakessoftware <aodude@icloud.com>
2024-08-24 23:09:32 -0700

    fixed signin stuff

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -1,18 +1,35 @@
-<script>
+<script lang="ts">
     import LogosGoogleIcon from '~icons/logos/google-icon';
-    import { onMount } from 'svelte';
+    import PhUserCircleLight from '~icons/ph/user-circle-light';
 
     let email = '';
     let password = '';
     let showPassword = false;
     let step = 1;
+    let emailValid = true;
 
     function nextStep() {
-        if (step === 1 && email) {
-            step = 2;
+        if (step === 1) {
+            if (isValidEmail(email)) {
+                emailValid = true;
+                step = 2;
+            } else {
+                emailValid = false;
+            }
         } else if (step === 2 && password) {
             window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
         }
+    }
+
+    function handleKeydown(event: KeyboardEvent) {
+        if (event.key === 'Enter') {
+            nextStep();
+        }
+    }
+
+    function isValidEmail(email: string): boolean {
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailPattern.test(email);
     }
 </script>
 
@@ -29,10 +46,7 @@
                     <h2 class="g text-3xl mt-6">Welcome</h2>
                     <div class="flex items-center mt-4">
                         <span class="border border-gray-300 rounded-full p-2">
-                            <!-- You can replace this with an actual icon if available -->
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12H8m0 0H4m4 0v8m4-8h4m-4-4v4m0 0V8m0-4h4v8M4 4h4m-4 4v8m4-8v8m4 0h4v8m-4-8H8m4-4V4m0 4v8m4 0v8m-4-8H8" />
-                            </svg>
+                            <PhUserCircleLight />
                         </span>
                         <p class="ml-2">{email}</p>
                     </div>
@@ -40,14 +54,20 @@
             </div>
             <div class="flex flex-col">
                 {#if step === 1}
-                    <input type="email" placeholder="Email" bind:value={email} class="border border-gray-500 rounded-sm p-3 w-64 mt-16 px-4" />
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        bind:value={email}
+                        class="border rounded-sm p-3 w-64 mt-16 px-4 ${emailValid ? 'border-gray-500' : 'border-red-500 bg-red-300'}"
+                        on:keydown={handleKeydown}
+                    />
                     <a class="text-blue-600 text-sm my-2" href="/signin">Forgot email?</a>
                 {/if}
                 {#if step === 2}
                     {#if showPassword}
-                        <input type="text" placeholder="Enter your password" bind:value={password} class="border border-gray-500 rounded-sm p-3 w-64 px-4 mt-8" />
+                        <input type="text" placeholder="Enter your password" on:keydown={handleKeydown} bind:value={password} class="border border-gray-500 rounded-sm p-3 w-64 px-4 mt-8" />
                     {:else}
-                        <input type="password" placeholder="Enter your password" bind:value={password} class="border border-gray-500 rounded-sm p-3 w-64 px-4 mt-8" />
+                        <input type="password" placeholder="Enter your password" on:keydown={handleKeydown} bind:value={password} class="border border-gray-500 rounded-sm p-3 w-64 px-4 mt-8" />
                     {/if}
                     <div class="flex items-center mt-2">
                         <input type="checkbox" bind:checked={showPassword} class="mr-2" />
